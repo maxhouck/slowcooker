92a4da87bb1de1fc95b6447dd03d15eece8c66dc
Max Houck <mmhww8@mail.missouri.edu>
2019-03-25 01:29:50 +0000

    everything set up for real this time

--- slowcooker_daemon.py
+++ slowcooker_daemon.py
@@ -1,12 +1,29 @@
 import json
-import RPi.GPIO as GPIO
-import time
 import fcntl
 import os
+import RPi.GPIO as GPIO
+import time
+
 from enum import Enum
 
 LOCK_PATH = "/home/pi/slowcooker/lockfile"
 lockfile = open(LOCK_PATH, "w+")
+
+def check_alarm(alarmtime):
+    currenttime = time.localtime(time.time())
+    if currenttime >= alarmtime :
+        print("alarm sounded ring ring ring")
+        status = Status.heating;
+    else:
+        print("no alarm yet")
+
+def update_coil():
+    status_dict["coil_status"] = GPIO.input(coil2_pin)
+    if status_dict["coil_activate"] != status_dict["coil_status"] : #if they dont match, change pins and reset coil_status
+        print("coil_status changed to", status_dict["coil_activate"])
+        GPIO.output(coil1_pin, status_dict["coil_activate"])
+        GPIO.output(coil2_pin, status_dict["coil_activate"])
+        status_dict["coil_status"] = GPIO.input(coil2_pin)
 
 while True:
     try:
@@ -28,20 +45,17 @@ GPIO.setup(coil2_pin, GPIO.OUT)
 GPIO.output(coil2_pin, GPIO.LOW)
 
 #load json file
-json_file =  open('slowcooker_status.json', 'r+')
+json_file =  open('slowcooker_status.json', 'r')
 status_dict = json.load(json_file)
+json_file.close()
+json_file =  open('slowcooker_status.json', 'w')
 for key in status_dict:
     print key
 
 if status_dict["device_status"] == "off":
     print("status is off")
+    update_coil()
 
-    status_dict["coil_status"] = GPIO.input(coil2_pin)
-    if status_dict["coil_activate"] != status_dict["coil_status"] : #if they dont match, change pins and reset coil_status
-        print("coil_status changed to", status_dict["coil_activate"])
-        GPIO.output(coil1_pin, status_dict["coil_activate"])
-        GPIO.output(coil2_pin, status_dict["coil_activate"])
-        status_dict["coil_status"] = GPIO.input(coil2_pin)
 elif status_dict["device_status"] == "waiting":
     print("status is waiting")
     #check alarm
@@ -53,55 +67,28 @@ elif status_dict["device_status"] == "waiting":
         #set a new alarm for when to turn off
     else:
         print("no alarm yet")
-
-    status_dict["coil_status"] = GPIO.input(coil2_pin)
-    if status_dict["coil_activate"] != status_dict["coil_status"] : #if they dont match, change pins and reset coil_status
-        print("coil_status changed to", status_dict["coil_activate"])
-        GPIO.output(coil1_pin, status_dict["coil_activate"])
-        GPIO.output(coil2_pin, status_dict["coil_activate"])
-        status_dict["coil_status"] = GPIO.input(coil2_pin)
+    update_coil()
 
 elif status_dict["device_status"] == "heating":
     print("status is HEATING")
     #check if alarm + cooktime has passed and set state to 1 if so
-
     #check temperature and set state to 3 or 4
-
-    status_dict["coil_status"] = GPIO.input(coil2_pin)
-    if status_dict["coil_activate"] != status_dict["coil_status"] : #if they dont match, change pins and reset coil_status
-        print("coil_status changed to", status_dict["coil_activate"])
-        GPIO.output(coil1_pin, status_dict["coil_activate"])
-        GPIO.output(coil2_pin, status_dict["coil_activate"])
-        status_dict["coil_status"] = GPIO.input(coil2_pin)
+    update_coil()
 
 elif status_dict["device_status"] == "cooling":
     print("status is COOLING")
     #check if alarm + cooktime has passed and set state to 1 if so
-
     #check temperature and set state to 3 or 4
+    update_coil()
 
-    status_dict["coil_status"] = GPIO.input(coil2_pin)
-    if status_dict["coil_activate"] != status_dict["coil_status"] : #if they dont match, change pins and reset coil_status
-        print("coil_status changed to", status_dict["coil_activate"])
-        GPIO.output(coil1_pin, status_dict["coil_activate"])
-        GPIO.output(coil2_pin, status_dict["coil_activate"])
-        status_dict["coil_status"] = GPIO.input(coil2_pin)
-
+elif status_dict["device_status"] == "debug":
+    print("status is debug")
+    update_coil()
 
 #write json
-json.dumps(status_dict, json_file)
-
+json.dump(status_dict, json_file, indent=4, sort_keys=True)
 fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
 
 lockfile.close()
 json_file.close()
 GPIO.cleanup()
-
-
-def check_alarm(alarmtime):
-    currenttime = time.localtime(time.time())
-    if currenttime >= alarmtime :
-        print("alarm sounded ring ring ring")
-        status = Status.heating;
-    else:
-        print("no alarm yet")
--- slowcooker_server.py
+++ slowcooker_server.py
@@ -1,149 +1,188 @@
-import RPi.GPIO as GPIO
 import time
 import datetime
 from flask import Flask, render_template, request, redirect
-import multiprocessing
-import time
+import json
+import fcntl
+import os
 from enum import Enum
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   24 : {'name' : 'coil 1', 'state' : GPIO.LOW},
-   25 : {'name' : 'coil 2', 'state' : GPIO.LOW}
-   }
-
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
-
 @app.route("/")
 def root():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('main.html', **templateData)
-
-@app.route("/slowcooker")
-def slowcooker():
-     # read status of two outputs and alarms
-     #output_status = get_output_status()
-     # For each pin, read the pin state and store it in the pins dictionary:
-        for pin in pins:
-            pins[pin]['state'] = GPIO.input(pin)
-         # Put the pin dictionary into the template data dictionary:
-        templateData = {
-            'pins' : pins
-            }
-         # Pass the template data into the template main.html and return it to the user
-        return render_template('main.html', **templateData)
+    os.system("python slowcooker_daemon.py")
+    LOCK_PATH = "/home/pi/slowcooker/lockfile"
+    lockfile = open(LOCK_PATH, "w+")
+    while True:
+       try:
+           fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+           break
+       except IOError as e:
+           # raise on unrelated IOErrors
+           if e.errno != errno.EAGAIN:
+               raise
+           else:
+               time.sleep(0.1)
+    json_file = open('slowcooker_status.json', 'r')
+    status_dict = json.load(json_file)
+    json_file.close()
+    fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
+    lockfile.close()
+    return render_template('main.html', status_dict = status_dict, time = time.time())
 
 @app.route("/cooknow", methods=["POST"])
 def cooknow():
     if request.method == 'POST':
-        cooktime = request.form['cooktime']
-        cooktemperature = request.form['temperature']
-        return redirect("/slowcooker")
+        LOCK_PATH = "/home/pi/slowcooker/lockfile"
+        lockfile = open(LOCK_PATH, "w+")
+        while True:
+            try:
+                fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                break
+            except IOError as e:
+                # raise on unrelated IOErrors
+                if e.errno != errno.EAGAIN:
+                    raise
+                else:
+                    time.sleep(0.1)
+        json_file =  open('slowcooker_status.json', 'r')
+        status_dict = json.load(json_file)
+        json_file.close()
+        json_file =  open('slowcooker_status.json', 'w')
+        print(status_dict["alarm"]["set"])
+        status_dict["device_status"]="heating"
+        status_dict["alarm"]["set"]=True
+        status_dict["coil_activate"]=True
+        status_dict["alarm"]["time"]=time.time() + int(request.form['cooktime'])*60
+        status_dict["temperature_target"]=request.form['temperature']
+        json.dump(status_dict, json_file, indent=4, sort_keys=True)
+        fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
+        lockfile.close()
+        json_file.close()
+        return redirect("/")
 
-@app.route("/toggle1", methods=["GET"])
-def toggle1():
-    print("hullaballoo")
-    #toggle_output(coil1_pin)
-    global coil1_status
-    coil1_status = not coil1_status
-    return redirect("/slowcooker")
+@app.route("/turnoff")
+def turnoff():
+    LOCK_PATH = "/home/pi/slowcooker/lockfile"
+    lockfile = open(LOCK_PATH, "w+")
+    while True:
+        try:
+            fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            break
+        except IOError as e:
+            # raise on unrelated IOErrors
+            if e.errno != errno.EAGAIN:
+                raise
+            else:
+                time.sleep(0.1)
+    json_file =  open('slowcooker_status.json', 'r')
+    status_dict = json.load(json_file)
+    json_file.close()
+    json_file =  open('slowcooker_status.json', 'w')
+    print(status_dict["alarm"]["set"])
+    status_dict["device_status"]="off"
+    status_dict["alarm"]["set"]=False
+    status_dict["coil_activate"]=False
+    status_dict["alarm"]["time"]=0
+    status_dict["temperature_target"]=0
+    json.dump(status_dict, json_file, indent=4, sort_keys=True)
+    fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
+    lockfile.close()
+    json_file.close()
+    return redirect("/")
 
-@app.route("/toggle2", methods=["GET"])
-def toggle2():
-    print("hullaballoo", coil2_status)
-    #toggle_output(coil2_pin)
-    global coil2_status
-    coil2_status = not coil2_status
-    print("hullaballoo", coil2_status)
-    return redirect("/slowcooker")
+@app.route("/cancel")
+def cancel():
+    LOCK_PATH = "/home/pi/slowcooker/lockfile"
+    lockfile = open(LOCK_PATH, "w+")
+    while True:
+        try:
+            fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            break
+        except IOError as e:
+            # raise on unrelated IOErrors
+            if e.errno != errno.EAGAIN:
+                raise
+            else:
+                time.sleep(0.1)
+    json_file =  open('slowcooker_status.json', 'r')
+    status_dict = json.load(json_file)
+    json_file.close()
+    json_file =  open('slowcooker_status.json', 'w')
+    print(status_dict["alarm"]["set"])
+    status_dict["device_status"]="off"
+    status_dict["alarm"]["set"]=False
+    status_dict["coil_activate"]=False
+    status_dict["alarm"]["time"]=0
+    status_dict["temperature_target"]=0
+    json.dump(status_dict, json_file, indent=4, sort_keys=True)
+    fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
+    lockfile.close()
+    json_file.close()
+    return redirect("/")
+
+@app.route("/toggle", methods=["GET"])
+def toggle():
+    LOCK_PATH = "/home/pi/slowcooker/lockfile"
+    lockfile = open(LOCK_PATH, "w+")
+    while True:
+        try:
+            fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            break
+        except IOError as e:
+            # raise on unrelated IOErrors
+            if e.errno != errno.EAGAIN:
+                raise
+            else:
+                time.sleep(0.1)
+    json_file =  open('slowcooker_status.json', 'r')
+    status_dict = json.load(json_file)
+    json_file.close()
+    json_file =  open('slowcooker_status.json', 'w')
+    status_dict["device_status"]="debug"
+    status_dict["alarm"]["set"]=False
+    status_dict["coil_activate"]=not status_dict["coil_activate"]
+    json.dump(status_dict, json_file, indent=4, sort_keys=True)
+    fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
+    lockfile.close()
+    json_file.close()
+    return redirect("/")
 
 @app.route("/setalarm", methods=["POST"])
 def setalarm():
     if request.method == 'POST':
-        alarmtime = request.form['alarmtime']
-        cooktime = request.form['cooktime']
-        cooktemperature = request.form['temperature']
-        print(alarmtime)
-        return redirect("/slowcooker")
-
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/<changePin>/<action>")
-def action(changePin, action):
-   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
-   # Get the device name for the pin being changed:
-   deviceName = pins[changePin]['name']
-   # If the action part of the URL is "on," execute the code indented below:
-   if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
-   if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
-   if action == "toggle":
-      # Read the pin and set it to whatever it isn't (that is, toggle it):
-      GPIO.output(changePin, not GPIO.input(changePin))
-      message = "Toggled " + deviceName + "."
-
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'message' : message,
-      'pins' : pins
-   }
-
-   return render_template('main.html', **templateData)
-
-class Status(Enum):
-    off = 1
-    alarm_set = 2
-    heating = 3
-    idle = 4
-
-#timer_cycle = (ontime:100, temperature:150)
-#alarm_cycle = (alarmtuple, ontime:100, temperature:150)
-
-
-
-def main_loop():
-    while 1:
-        #alarmtime = (2019, 2, 27, 5, 41, 21, 3, 1, 0)
-        #check_alarm(alarmtime)
-        #alarmtime = (2019, 2, 27, 6, 41, 21, 3, 1, 0)
-        #check_alarm(alarmtime)
-        update_outputs()
-
-coil1_status = False
-coil2_status = False
-coil1_pin = 24
-coil2_pin = 25
-alarmset = 0
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(coil1_pin, GPIO.OUT)
-GPIO.output(coil1_pin, GPIO.LOW)
-GPIO.setup(coil2_pin, GPIO.OUT)
-GPIO.output(coil2_pin, GPIO.LOW)
-
-#p1 = multiprocessing.Process(target=main_loop, args=())
-#p1.start()
+        print(request.form['alarmtime'])
+        LOCK_PATH = "/home/pi/slowcooker/lockfile"
+        lockfile = open(LOCK_PATH, "w+")
+        while True:
+            try:
+                fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                break
+            except IOError as e:
+                # raise on unrelated IOErrors
+                if e.errno != errno.EAGAIN:
+                    raise
+                else:
+                    time.sleep(0.1)
+        json_file =  open('slowcooker_status.json', 'r')
+        status_dict = json.load(json_file)
+        json_file.close()
+        json_file =  open('slowcooker_status.json', 'w')
+        print(status_dict["alarm"]["set"])
+        status_dict["device_status"]="waiting"
+        status_dict["alarm"]["set"]=True
+        split = request.form['alarmtime'].split(':')
+        hours = int(split[0])
+        minutes = int(split[1])
+        alarmtime = time.time() + hours*3600 + minutes*60
+        status_dict["alarm"]["time"]=alarmtime
+        status_dict["cooktime"]=request.form['cooktime']
+        status_dict["temperature_target"]=request.form['temperature']
+        print(status_dict["alarm"]["set"])
+        json.dump(status_dict, json_file, indent=4, sort_keys=True)
+        fcntl.lockf(lockfile, fcntl.LOCK_UN) #release lockfile
+        lockfile.close()
+        json_file.close()
+    return redirect("/")
 
 app.run(host='0.0.0.0', port=80, debug=True)
